9f4ab29719ed1980042d1d04668ee8a275afffe4
meyouwe123 <37802965+meyouwe123@users.noreply.github.com>
2018-05-07 11:51:44 +0200

    Create galaxy.js

---
+++ galaxy.js
@@ -0,0 +1,36 @@
+let mybubble;
+let shp;
+let stars;
+let time;
+let speedoftime;
+let goal;
+
+let shipdata = {ship: "Millenium Falcon",
+                  journey: []
+                }
+                
+let places = []
+
+function preload() {
+  map = loadImage("...")
+ }
+ 
+function setup() {
+  createCanvas(800,600);
+  mybubble = new Bubble(width/2, height/2, shp, shipdata);
+  image(stars, 0, 0,stars.width/2, stars.height/2);
+  time = 0;
+  speedoftime = 2;
+  goal = 0;
+}
+
+function draw(){
+  image(stars, 0, 0,stars.width/2, stars.height/2);
+  goal = mybubble.intransit(time);
+  if (goal[0]){
+    mybubble.move(mybubble.data.journey[goal[1]].destination[0], mybubble.data.journey[goal[1]].destination[1]);
+  } 
+  
+  mybubble.display();
+  time = timeflow(time, speedoftime);
+}
