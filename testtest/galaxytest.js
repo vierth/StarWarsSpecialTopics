1c6afe416a58bc8139d9be8628dab186df3c8a21
annaj20 <37802973+annaj20@users.noreply.github.com>
2018-05-14 11:57:02 +0200

    Update galaxytest.js

--- testtest/galaxytest.js
+++ testtest/galaxytest.js
@@ -31,7 +31,7 @@ function preload(){
 
 
 function setup(){
-  createCanvas(1263,902)
+  createCanvas(8000,6000)
   for (i = 0; i < data.getRowCount(); i++){
     // get the data from table
     ship_id = data.getNum(i,0)
