c0f0a0aecb2499899df240607e29743ccd881e3e
annaj20 <37802973+annaj20@users.noreply.github.com>
2018-05-14 12:10:54 +0200

    Update galaxytest.js

--- testtest/galaxytest.js
+++ testtest/galaxytest.js
@@ -60,7 +60,7 @@ function setup(){
   }
 
   // Set values for data, and display the background image
-  image(stars, 0, 0,stars.width/2, stars.height/2);
+  image(stars, 0, 0);
   time = 1;
   startingframes = time * 60;
   speedoftime = 1;
@@ -72,7 +72,7 @@ function setup(){
 function draw(){
   // Draw background
   imageMode(CORNER);
-  image(stars, 0, 0,stars.width/2, stars.height/2);
+  image(stars, 0, 0);
 
   // Update each ship
   for (i = 0; i < ships.length; i++){
